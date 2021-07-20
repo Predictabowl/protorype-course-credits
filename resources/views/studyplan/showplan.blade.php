91639cb47c52f869c512db5b388c9b9cc024bdfe
Piero <piero.aldinucci@gmail.com>
2021-07-20 14:30:56 +0200

    soft deleted full.blade.php Time to test some front-end

--- resources/views/studyplan/showplan.blade.php
+++ resources/views/studyplan/showplan.blade.php
@@ -1,41 +1,48 @@
-<x-full-layout>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Integrazione') }}
+        </h2>
+    </x-slot>
 
-    <x-panel>
-        <h1>Crediti Assegnati</h1>
-        <div class="place-content-center">
-            <table class="min-w-full rounded-lg">
-                <thead>
-                    <tr class="bg-gray-100">
-                        <th>ssd</th>
-                        <th>Nome</th>
-                        <th>CFU</th>
-                        <th>Compatibilità</th>
-                        <th>Esami riconosciuti</th>
-                        <th>Integrazione</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($studyPlan->getExams() as $exam)
-                    <tr class="hover:bg-blue-100">
-                        <td>{{ $exam->getExamOption()->getSsd() }}</td>
-                        <td>{{ $exam->getExamOption()->getExamName() }}</td>
-                        <td class="text-center">{{ $exam->getExamOption()->getCfu() }}</td>
-                        <td>
-                            @foreach($exam->getExamOption()->getCompatibleOptions() as $option)
-                                {{$option}},
-                            @endforeach
-                        </td>
-                        <td>
-                            @foreach($exam->getTakenExams() as $taken)
-                                {{$taken->getTakenExam()->getSsd()}}: {{ $taken->getActualCfu()}}/{{$taken->getTakenExam()->getCfu()}}, 
-                            @endforeach
-                        </td>
-                        <td>{{ $exam->getIntegrationValue()}}</td>
+    <x-mainpanel>
+        <x-panel>
+            <h1>Crediti Assegnati</h1>
+            <div class="place-content-center">
+                <table class="min-w-full rounded-lg">
+                    <thead>
+                        <tr class="bg-gray-100">
+                            <th>ssd</th>
+                            <th>Nome</th>
+                            <th>CFU</th>
+                            <th>Compatibilità</th>
+                            <th>Esami riconosciuti</th>
+                            <th>Integrazione</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($studyPlan->getExams() as $exam)
+                        <tr class="hover:bg-blue-100">
+                            <td>{{ $exam->getExamOption()->getSsd() }}</td>
+                            <td>{{ $exam->getExamOption()->getExamName() }}</td>
+                            <td class="text-center">{{ $exam->getExamOption()->getCfu() }}</td>
+                            <td>
+                                @foreach($exam->getExamOption()->getCompatibleOptions() as $option)
+                                    {{$option}},
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach($exam->getTakenExams() as $taken)
+                                    {{$taken->getTakenExam()->getSsd()}}: {{ $taken->getActualCfu()}}/{{$taken->getTakenExam()->getCfu()}}, 
+                                @endforeach
+                            </td>
+                            <td>{{ $exam->getIntegrationValue()}}</td>
 
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </x-panel>
-</x-full-layout>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </x-panel>
+    </x-mainpanel>
+</x-app-layout>
