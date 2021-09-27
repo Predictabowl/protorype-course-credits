3514eef25c43feb72dff68eeddc44f490a428d63
Piero <piero.aldinucci@gmail.com>
2021-09-27 15:41:37 +0200

    corrected legal heading

--- resources/views/components/legal-heading.blade.php
+++ resources/views/components/legal-heading.blade.php
@@ -1,6 +1,8 @@
 <p style="margin-bottom: 1rem;">
-    La presente bozza di riconoscimento esami è predisposta dagli Uffici di Segreteria Didattica del Dipartimento di Giurisprudenza sulla base dei dati autodichiarati dagli studenti. Essa è subordinata all’approvazione da parte della Presidente del corso di laurea, nonché alla correttezza e alla completezza delle informazioni (CFU, nome dell'insegnamento, SSD, etc.) contenute nell'autocertificazione trasmessa agli Uffici.
-
+    La presente bozza di riconoscimento esami è predisposta dagli Uffici di Segreteria Didattica del Dipartimento di Giurisprudenza sulla base dei dati autodichiarati dagli studenti. Essa è subordinata all’approvazione da parte della Presidente del corso di laurea, nonché alla correttezza e alla completezza delle informazioni (CFU, nome dell'insegnamento, SSD, etc.) contenute nell'autocertificazione trasmessa agli Uffici. 
+    <span style="font-weight: bold;">
+        Nel caso di informazioni incomplete o incorrette, il presente riconoscimento dovrà essere adeguato, in sede di caricamento degli esami, agli esami effettivamente sostenuti, con i relativi CFU e SSD.
+    </span>
 </p>
 <p>
     Pertanto, si invitano gli studenti a controllare con molta attenzione il contenuto autodichiarato, per verificare che i dati imputati durante la compilazione del modulo corrispondano esattamente agli insegnamenti sostenuti (denominazione, CFU e SSD) e dei quali si richiede il riconoscimento.
