313cd830926735e0879243ed9a0ef49d7586e69b
Piero Aldinucci <piero.aldinucci@gmail.com>
2022-12-30 14:21:53 +0100

    Added Heroicons

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -14,13 +14,16 @@
 
             <div class="place-content-center">
                 <table class="min-w-full rounded-lg">
+                    <caption>Lista dei corsi di laurea disponibili</caption>
                     <thead>
                         <tr class="bg-gray-100">
                             <th> {{ __("Student") }}</th>
                             <th class="flex place-content-center">
                                 <x-dropdown align="right" width="w-max">
                                     <x-slot name="trigger">
-                                        <button class="flex items-center text-sm hover:text-blue-600 text-gray-500 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                        <button class="flex items-center text-sm hover:text-blue-600
+                                         text-gray-500 focus:outline-none focus:text-gray-700 focus:border-gray-300
+                                          transition duration-150 ease-in-out">
                                             <div class="font-bold text-base text-black hover:text-blue-600">
                                                 {{ __("Course") }}:
                                             </div>
@@ -30,7 +33,8 @@
                                                 </div>
                                             @endif
                                             <div class="ml-1">
-                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                                 </svg>
                                             </div>
